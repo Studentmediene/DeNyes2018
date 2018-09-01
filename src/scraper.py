ab292cd00fe5246a05d489d569aa5b251a02f175
Kristian Flatheim Jensen <kristianflatheim@hotmail.com>
2018-09-01 15:29:22 +0200

    Added error handling for misformatted XML

--- src/scraper.py
+++ src/scraper.py
@@ -4,17 +4,22 @@ from time import sleep
 def fetch():
     url = "http://www.dusken.no/feed"
     content = feedparser.parse(url)
-    return content["entries"]
+    if "entries" in content.keys():
+        return content["entries"]
+    else:
+        return None
 
 def diff_entries(old_entries, new_entries):
     for entry in new_entries:
-        title = entry["guid"]
-        if title not in [e["guid"] for e in old_entries]:
-            yield entry
+        if "guid" in entry.keys():
+            title = entry["guid"]
+            if title not in [e["guid"] for e in old_entries]:
+                yield entry
 
 def entries_to_messages(entries):
     for entry in entries:
-        yield entry["title"] + "\n" + entry["link"]
+        if "title" in entry.keys() and "link" in entry.keys():
+            yield entry["title"] + "\n" + entry["link"]
 
 def send_message(message):
     pass
@@ -25,7 +30,8 @@ def get_new_entries():
         sleep(1000)
         old_entries = new_entries[:]
         new_entries = fetch()
-        diff = diff_entries(old_entries, new_entries)
-        messages = entries_to_messages(diff)
-        for msg in messages:
-            yield msg
+        if new_entries is not None:
+            diff = diff_entries(old_entries, new_entries)
+            messages = entries_to_messages(diff)
+            for msg in messages:
+                yield msg
