2766e559f45ca7caa789e065114965c9182bdab4
Simon B-J <simon_bj@live.com>
2018-09-01 16:36:43 +0200

    Added event listener

--- src/app.py
+++ src/app.py
@@ -1,8 +1,9 @@
-from flask import Flask
+import json
+
+from flask import Flask, request, make_response
 from flask_apscheduler import APScheduler
-from client import send_message
-from scraper import fetch, get_new_entries
-from time import sleep
+from client import send_message, token
+from scraper import fetch, get_new_entries, entries_to_messages
 
 
 app = Flask(__name__)
@@ -29,6 +30,27 @@ def post_new_entries():
     for msg in get_new_entries(existing_entries, new_entries):
         send_message(msg)
 
+
+@app.route('/events', methods=['GET', 'POST'])
+def derp():
+    slack_event = json.loads(request.data)
+
+    if "challenge" in slack_event:
+        return make_response(slack_event["challenge"], 200, {"content_type": "application/json" })
+
+    if token != slack_event.get("token"):
+        message = "Invalid Slack verification token: %s \npyBot has: \
+                       %s\n\n" % (slack_event["token"], token.verification)
+
+        make_response(message, 403, {"X-Slack-No-Retry": 1})
+
+    if "event" in slack_event:
+        return send_message(entries_to_messages(fetch()[:1])[0])
+
+    return make_response("[NO EVENT IN SLACK REQUEST] These are not the droids\
+                             you're looking for.", 404, {"X-Slack-No-Retry": 1})
+
+
 if __name__ == '__main__':
     app.config.from_object(Config())
 
@@ -36,5 +58,4 @@ if __name__ == '__main__':
     scheduler.init_app(app)
     scheduler.start()
 
-    app.run()
-
+    app.run(host='0.0.0.0', port=80)
--- src/client.py
+++ src/client.py
@@ -3,8 +3,11 @@ import os
 from slackclient import SlackClient
 
 
+token = os.environ['SLACK_API_TOKEN']
+
+
 def send_message(msg):
-    client = SlackClient(os.environ['SLACK_API_TOKEN'])
+    client = SlackClient(token)
     return client.api_call(
       "chat.postMessage",
       channel="#de-nyes",
